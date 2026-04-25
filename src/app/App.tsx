bf829e94dfb565ea9b8b980cfae6b6364d9dcaae
kajalvermaaa <kajalverma1104@gmail.com>
2026-04-25 20:05:59 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,9 @@
-import { CustomCursor } from "./CustomCursor";
 import { TheHuman } from "./TheHuman";
-// import other sections as you build them, e.g.:
-// import { Hero } from "./Hero";
-// import { Work } from "./Work";
-// import { Contact } from "./Contact";
 
 export default function App() {
   return (
     <>
-      <CustomCursor />
-      {/* Add your sections in order */}
-      {/* <Hero /> */}
       <TheHuman />
-      {/* <Work /> */}
-      {/* <Contact /> */}
     </>
   );
 }
