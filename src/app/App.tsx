5100aa6d9219b6a51ba0fb8ff3c943405aecb8ac
kajalvermaaa <kajalverma1104@gmail.com>
2026-04-25 20:03:17 +0530

    Include TheHuman component in App
    Added TheHuman component to the App.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
 import { CustomCursor } from "./CustomCursor";
+import { TheHuman } from "./TheHuman";
+// import other sections as you build them, e.g.:
+// import { Hero } from "./Hero";
+// import { Work } from "./Work";
+// import { Contact } from "./Contact";
 
 export default function App() {
   return (
     <>
       <CustomCursor />
-      {/* rest of your app */}
+      {/* Add your sections in order */}
+      {/* <Hero /> */}
+      <TheHuman />
+      {/* <Work /> */}
+      {/* <Contact /> */}
     </>
   );
 }
